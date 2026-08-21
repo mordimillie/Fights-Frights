4934abb379430a8abb63ba392be89588dddd771b
mordimillie <robinprocko@gmail.com>
2026-08-20 22:30:42 -0700

    Add files via upload

---
+++ src/main/java/net/mcreator/fightsfrights/recipes/brewing/InfectionPCraftBrewingRecipe.java
@@ -0,0 +1,45 @@
+
+package net.mcreator.fightsfrights.recipes.brewing;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.alchemy.Potions;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+
+import net.mcreator.fightsfrights.init.FightsfrightsModPotions;
+import net.mcreator.fightsfrights.init.FightsfrightsModItems;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class InfectionPCraftBrewingRecipe implements IBrewingRecipe {
+	@SubscribeEvent
+	public static void init(FMLCommonSetupEvent event) {
+		event.enqueueWork(() -> BrewingRecipeRegistry.addRecipe(new InfectionPCraftBrewingRecipe()));
+	}
+
+	@Override
+	public boolean isInput(ItemStack input) {
+		Item inputItem = input.getItem();
+		return (inputItem == Items.POTION || inputItem == Items.SPLASH_POTION || inputItem == Items.LINGERING_POTION) && PotionUtils.getPotion(input) == Potions.AWKWARD;
+	}
+
+	@Override
+	public boolean isIngredient(ItemStack ingredient) {
+		return Ingredient.of(new ItemStack(FightsfrightsModItems.PATHOGEN.get())).test(ingredient);
+	}
+
+	@Override
+	public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
+		if (isInput(input) && isIngredient(ingredient)) {
+			return PotionUtils.setPotion(new ItemStack(input.getItem()), FightsfrightsModPotions.INFECTION_P.get());
+		}
+		return ItemStack.EMPTY;
+	}
+}
