6ba00e78d3f4017664e08a4647ef62ed3a1b72d7
mordimillie <robinprocko@gmail.com>
2026-08-20 22:28:00 -0700

    Add files via upload

---
+++ src/main/java/net/mcreator/fightsfrights/network/TrashcanGUIButtonMessage.java
@@ -0,0 +1,75 @@
+
+package net.mcreator.fightsfrights.network;
+
+import net.minecraftforge.network.NetworkEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.fightsfrights.world.inventory.TrashcanGUIMenu;
+import net.mcreator.fightsfrights.procedures.TrashingProcedure;
+import net.mcreator.fightsfrights.FightsfrightsMod;
+
+import java.util.function.Supplier;
+import java.util.HashMap;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class TrashcanGUIButtonMessage {
+	private final int buttonID, x, y, z;
+
+	public TrashcanGUIButtonMessage(FriendlyByteBuf buffer) {
+		this.buttonID = buffer.readInt();
+		this.x = buffer.readInt();
+		this.y = buffer.readInt();
+		this.z = buffer.readInt();
+	}
+
+	public TrashcanGUIButtonMessage(int buttonID, int x, int y, int z) {
+		this.buttonID = buttonID;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public static void buffer(TrashcanGUIButtonMessage message, FriendlyByteBuf buffer) {
+		buffer.writeInt(message.buttonID);
+		buffer.writeInt(message.x);
+		buffer.writeInt(message.y);
+		buffer.writeInt(message.z);
+	}
+
+	public static void handler(TrashcanGUIButtonMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
+		NetworkEvent.Context context = contextSupplier.get();
+		context.enqueueWork(() -> {
+			Player entity = context.getSender();
+			int buttonID = message.buttonID;
+			int x = message.x;
+			int y = message.y;
+			int z = message.z;
+			handleButtonAction(entity, buttonID, x, y, z);
+		});
+		context.setPacketHandled(true);
+	}
+
+	public static void handleButtonAction(Player entity, int buttonID, int x, int y, int z) {
+		Level world = entity.level();
+		HashMap guistate = TrashcanGUIMenu.guistate;
+		// security measure to prevent arbitrary chunk generation
+		if (!world.hasChunkAt(new BlockPos(x, y, z)))
+			return;
+		if (buttonID == 0) {
+
+			TrashingProcedure.execute(world, x, y, z, entity);
+		}
+	}
+
+	@SubscribeEvent
+	public static void registerMessage(FMLCommonSetupEvent event) {
+		FightsfrightsMod.addNetworkMessage(TrashcanGUIButtonMessage.class, TrashcanGUIButtonMessage::buffer, TrashcanGUIButtonMessage::new, TrashcanGUIButtonMessage::handler);
+	}
+}
